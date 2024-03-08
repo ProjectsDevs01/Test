d711f19aa4a5622fbc8550a9502285b3c8a7494f
ProjectsDevs01 <codingdevs11@gmail.com>
2024-03-08 10:56:54 +0530

    removed comment

--- oms/src/main/java/com/codingdevs/oms/controller/customer/CustomerController.java
+++ oms/src/main/java/com/codingdevs/oms/controller/customer/CustomerController.java
@@ -7,7 +7,6 @@ import com.codingdevs.oms.service.customer.CustomerService;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
