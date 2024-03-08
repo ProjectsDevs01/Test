5c6cc2242c953e1bfc934c0d6e6e0ef779800eaa
ProjectsDevs01 <codingdevs11@gmail.com>
2024-03-08 13:07:29 +0530

    git bug

--- oms/src/main/java/com/codingdevs/oms/controller/products/ProductController.java
+++ oms/src/main/java/com/codingdevs/oms/controller/products/ProductController.java
@@ -12,10 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-<<<<<<< HEAD
-=======
 import org.springframework.web.bind.annotation.PutMapping;
->>>>>>> api
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
