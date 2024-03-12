52c53c3a989fc5fcf59e07c1e01d5ba95ecdb0c3
ProjectsDevs01 <codingdevs11@gmail.com>
2024-03-13 00:57:05 +0530

    pdf controller is added

---
+++ oms/src/main/java/com/codingdevs/oms/controller/products/PdfController.java
@@ -0,0 +1,34 @@
+package com.codingdevs.oms.controller.products;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codingdevs.oms.service.products.PdfService;
+
+@RestController
+@RequestMapping("/api/pdf")
+public class PdfController {
+
+	@Autowired
+	private PdfService pdfService;
+
+	@PostMapping("/convert")
+	public ResponseEntity<Resource> convertPdf(@RequestBody String htmlString) throws Exception {
+		byte[] pdfBytes = pdfService.converPdf(htmlString);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_PDF);
+		headers.setContentDispositionFormData("attachment", "pdf.pdf");
+		ByteArrayResource resource = new ByteArrayResource(pdfBytes);
+
+		return ResponseEntity.ok().headers(headers).body(resource);
+	}
+}
